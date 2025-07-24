85ab05ae338518bf162e56a747caf07ef3613561
mayrars <mayra203@gmail.com>
2025-07-24 05:54:06 -0600

    Adding menssages for registration and account verification

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,14 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 def register(request):
     if request.method == 'POST':
@@ -17,8 +25,20 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration Successful')
-            return redirect('register')
+
+            current_site= get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email =  EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            #messages.success(request, 'Thank you for registering with us. A verification email has been sent to your email address. Please verify your account.')
+            return redirect('/accounts/login/?command=verification&email=' + email)
 
     else:
         form = RegistrationForm()
@@ -28,19 +48,29 @@ def register(request):
     return render(request, 'accounts/register.html', context)
 
 
+# Define a function called login that takes in a request as an argument
 def login(request):
+    # Check if the request method is POST
     if request.method=='POST':
+        # Get the email and password from the request
         email = request.POST['email']
         password = request.POST['password']
 
+        # Authenticate the user with the email and password
         user = auth.authenticate(email=email, password=password)
 
+        # If the user is authenticated
         if user is not None:
+            # Log the user in
             auth.login(request, user)
+            # Redirect the user to the home page
             return redirect('home')
         else:
+            # If the user is not authenticated, display an error message
             messages.error(request, 'Invalid login credentials')
+            # Redirect the user to the login page
             return redirect('login')
+    # If the request method is not POST, render the login page
     return render(request, 'accounts/login.html')
 
 @login_required(login_url='login')
@@ -48,3 +78,19 @@ def logout(request):
     auth.logout(request) 
     messages.success(request, 'You are logged out')
     return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your account has been activated successfully')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
--- ecommerceKart/settings.py
+++ ecommerceKart/settings.py
@@ -137,3 +137,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mayra203@gmail.com'
+EMAIL_HOST_PASSWORD = 'wumi ulaw aadj eskp'
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
