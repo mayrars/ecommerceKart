c951db7bcd342f496b77842b9e270c39f6558718
mayrars <mayra203@gmail.com>
2025-07-30 05:37:44 -0600

    adding view and link to reset password

--- accounts/views.py
+++ accounts/views.py
@@ -138,3 +138,21 @@ def resetpassword_validate(request, uidb64, token):
     else:
         messages.error(request, 'This link has expired')
         return redirect('login')
+
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successfully')
+            return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
