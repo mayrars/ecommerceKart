0c9eb3fa6e05a35e36335023e9b92932b85649c7
mayrars <mayra203@gmail.com>
2025-08-01 05:32:06 -0600

    adding try to login

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,8 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
 
 def register(request):
     if request.method == 'POST':
@@ -60,6 +62,19 @@ def login(request):
 
         # If the user is authenticated
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+
+            except:
+                print("Cart does not exist")
+                pass
             # Log the user in
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
